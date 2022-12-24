50cfa0078c78a00d2849fc5fc5afb285f4e970c6
tartaric_acid <baka943@qq.com>
2022-12-24 23:13:02 +0800

    add config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Yes Steve Model Wiki',
+    description: 'There is a wiki related to Yes Steve Model mod',
+    base: '/ysm/',
+    markdown: {
+        lineNumbers: true
+    }
+}
